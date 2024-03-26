b923f623b7b803b70dff35ef613da0e7b1942bb0
Vynokur Yurii <vynokur.yurii@gmail.com>
2024-03-27 00:04:34 +0200

    Add base css

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,9 @@
+import {AppNav} from "../components/AppNav.jsx";
+
+export function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import {PageNav} from "../components/PageNav.jsx";
+import {Link} from "react-router-dom";
+import {AppNav} from "../components/AppNav.jsx";
 
 export function Home() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
+
+      <Link to="/app">App</Link>
     </div>
   );
 }
